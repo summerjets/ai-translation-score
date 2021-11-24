d64a07c6f161aed27289285e46c452cb6762d945
EC2 Default User <ec2-user@ip-172-31-88-157.ec2.internal>
2021-11-24 16:20:05 +0000

    Integrating model and requirements

--- flask_app/app.py
+++ flask_app/app.py
@@ -2,12 +2,19 @@ from flask import Flask
 from flask import request, jsonify
 from random import sample
 
+import torch
+torch.set_num_threads(1)
+
+from sentence_transformers import SentenceTransformer
+model = SentenceTransformer('paraphrase-albert-small-v2', device="cpu")
+
 server = Flask(__name__)
 
 @server.route('/', methods=['GET', 'POST'])
 def hello_world():
     if request.method == 'GET':
-        return 'This is a GET request'
+        _test = model.encode(["warmup test"])
+        return f"This is a GET request {_test}"
     else:
         return "This is not a GET request"
 
